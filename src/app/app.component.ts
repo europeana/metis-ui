367a83fdef624c060418f88cf0bf0d48a789ee99
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2017-10-04 12:16:38 +0200

    add compass and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
-import { RegisterComponent } from './register/register.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   
 }
