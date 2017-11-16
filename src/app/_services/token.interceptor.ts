dc1a3f931f4889aa3897b2705c368c859a56e1c7
Kiffin Gish <kiffin.gish@planet.nl>
2017-11-16 13:36:35 +0100

    Token not checked if login or register.

--- src/app/_services/token.interceptor.ts
+++ src/app/_services/token.interceptor.ts
@@ -20,15 +20,18 @@ export class TokenInterceptor implements HttpInterceptor {
   private auth;
   constructor(private inj: Injector) {}
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const auth = this.inj.get(AuthenticationService);
-    const token = auth.getToken();
-    if (token) {
-      const headers = { Authorization: `Bearer ${token}` };
-      console.log(`Token interceptor => ${JSON.stringify(headers)}`);
+    // Ignore if login, register or refresh.
+    if (!request.url.match(/login|register/)) {
+      const auth = this.inj.get(AuthenticationService);
+      const token = auth.getToken();
+      if (token) {
+        const headers = { Authorization: `Bearer ${token}` };
+        console.log(`Token interceptor => ${JSON.stringify(headers)}`);
         // Insert authorization header into all outgoing calls
         request = request.clone({
           setHeaders: headers
         });
+      }
     }
 
     return next.handle(request);
