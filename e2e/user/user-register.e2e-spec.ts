e68a314bf6f6d8a24974ecd764659f7bc82237c0
Kiffin Gish <kiffin.gish@planet.nl>
2017-11-28 11:58:47 +0100

    More tests user-register: all tests green.

---
+++ e2e/user/user-register.e2e-spec.ts
@@ -0,0 +1,52 @@
+import { RegisterPage } from '../pages/register/register.po';
+
+describe('User | Register', () => {
+  let page: RegisterPage;
+
+  beforeEach(() => {
+    page = new RegisterPage();
+    page.navigateTo();
+  });
+
+  it('should display form title', () => {
+    expect(page.getFormTitle()).toEqual('Register to Metis');
+  });
+
+  describe('submit button should be disabled', () => {
+    it('on page load', () => {
+      expect(page.getSubmitButton().isEnabled()).toBe(false);
+    });
+
+    it('when email filled but password empty', () => {
+      page.fillEmail('mirjam.metis@europeana.eu');
+      expect(page.getSubmitButton().isEnabled()).toBe(false);
+    });
+
+    it('when password filled but email empty', () => {
+      page.fillPassword('secret');
+      expect(page.getSubmitButton().isEnabled()).toBe(false);
+    });
+
+    it('when password filled but email is invalid', () => {
+      page.fillEmail('mirjam.metis');
+      page.fillPassword('secret');
+      expect(page.getSubmitButton().isEnabled()).toBe(false);
+    });
+
+    it('when email valid but passwords do not match', () => {
+      page.fillEmail('mirjam.metis@europeana.eu');
+      page.fillPassword('secret');
+      page.fillConfirm('secretx');
+      expect(page.getSubmitButton().isEnabled()).toBe(false);
+    });
+  });
+
+  describe('submit button should be enabled', () => {
+    it('when valid email and passwords match', () => {
+      page.fillEmail('mirjam.metis@europeana.eu');
+      page.fillPassword('secret');
+      page.fillConfirm('secret');
+      expect(page.getSubmitButton().isEnabled()).toBe(true);
+    });
+  });
+});
