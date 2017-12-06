6449c2da90ea5d6083e6bfdc848321e768cce747
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2017-12-06 16:21:49 +0100

    updated test

--- src/app/dataset/history/history.component.spec.ts
+++ src/app/dataset/history/history.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DatasetsService } from '../../_services';
 
 import { HistoryComponent } from './history.component';
 
@@ -8,7 +10,9 @@ describe('HistoryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HistoryComponent ]
+      imports: [ RouterTestingModule],
+      declarations: [ HistoryComponent ],
+      providers: [ DatasetsService ]
     })
     .compileComponents();
   }));
@@ -23,3 +27,4 @@ describe('HistoryComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
