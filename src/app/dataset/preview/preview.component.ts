2b1272e459abaff809e3e56a619087692c6c8891
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2018-02-14 17:01:19 +0100

    update selected workflow per sample

--- src/app/dataset/preview/preview.component.ts
+++ src/app/dataset/preview/preview.component.ts
@@ -33,6 +33,7 @@ export class PreviewComponent implements OnInit {
   allWorkflows;
   filterWorkflow: boolean = false;
   selectedWorkflow: string;
+  selectedWorkflowSamples: array = [];
   filterWorkflowSample: boolean = false;
   displaySearch: boolean = false;
   minRandom: number = 1;
@@ -59,10 +60,14 @@ export class PreviewComponent implements OnInit {
 
   }
 
-  getXMLSample(mode?, workflow?, keyword?) {
+  getXMLSample(mode?, workflow?, keyword?, index?) {
     this.editorPreviewCode = undefined;
     this.editorPreviewTitle = undefined;
-    this.selectedWorkflow = undefined;
+
+    if (typeof index !== 'number') {
+      this.selectedWorkflow = undefined;
+      this.selectedWorkflowSamples = [];
+    }
 
     let previewSample;
 
@@ -72,14 +77,22 @@ export class PreviewComponent implements OnInit {
     } else if (mode === 'workflow' && workflow !== '') {
       previewSample = this.filterPreviewWorkflow(workflow);
       this.displaySearch = false;
-      this.selectedWorkflow = workflow;
+      if (typeof index === 'number') {
+        this.selectedWorkflowSamples[index] = workflow;
+      } else {
+        this.selectedWorkflow = workflow;
+      }
     } else if (mode === 'search' && keyword != '') {
       previewSample = this.searchPreview(keyword); 
     }
-
+ 
     this.editorPreviewCode = previewSample.sample;
     this.editorPreviewTitle = previewSample.name;
     this.onClickedOutside();
+  }
+
+  getXMLThisSample(, mode?, workflow?, keyword?) {
+    this.getXMLSample(mode, workflow, keyword);
   }
 
   filterPreviewWorkflow(w) {
