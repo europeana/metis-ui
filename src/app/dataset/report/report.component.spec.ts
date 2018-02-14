a7588090854db274bd21be64d1e563e307dec672
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2018-02-14 12:08:56 +0100

    update report, add additional workflow option to test encorce plugin

--- src/app/dataset/report/report.component.spec.ts
+++ src/app/dataset/report/report.component.spec.ts
@@ -1,19 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { BaseRequestOptions, ConnectionBackend, Http, RequestOptions } from '@angular/http';
 
 import { ReportComponent } from './report.component';
 import { WorkflowService, AuthenticationService } from '../../_services';
+import {MockBackend, MockConnection} from '@angular/http/testing';
 
 describe('ReportComponent', () => {
   let component: ReportComponent;
   let fixture: ComponentFixture<ReportComponent>;
+  let spy: any;
+
+  const mockHttpProvider = {  
+    deps: [ MockBackend, BaseRequestOptions ],
+    useFactory: (backend: MockBackend, defaultOptions: BaseRequestOptions) => {
+      return new Http(backend, defaultOptions);
+    }
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ RouterTestingModule, HttpClientModule],
       declarations: [ ReportComponent ],
-      providers: [ WorkflowService, AuthenticationService ]
+      providers: [ WorkflowService, AuthenticationService, MockBackend ]
     })
     .compileComponents();
   }));
@@ -22,6 +32,12 @@ describe('ReportComponent', () => {
     fixture = TestBed.createComponent(ReportComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should close the report window', () => {
+    component.closeReport();
+    fixture.detectChanges();
+    expect(component.report).toEqual(undefined);
   });
 
   it('should create', () => {
