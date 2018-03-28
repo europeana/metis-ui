a32923861e76d0320137561d96fdc93d109b2d56
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2018-03-28 15:26:22 +0200

    remove unused codemirror code

--- src/app/dataset/mapping/mapping.component.ts
+++ src/app/dataset/mapping/mapping.component.ts
@@ -29,8 +29,6 @@ export class MappingComponent implements OnInit {
     private translate: TranslateService) { }
 
   @Input() datasetData: any;
-  defaultEditorConfig;
-  editorConfig;
   editorConfigEdit;
   statistics;
   statisticsMap = new Map();
@@ -47,13 +45,13 @@ export class MappingComponent implements OnInit {
   expandedStatistics: boolean;
 
   ngOnInit() {
-  	 this.defaultEditorConfig = { 
-      mode: 'application/json',
+  	 this.editorConfigEdit = { 
+      mode: 'application/xml',
       lineNumbers: true,
       indentUnit: 2,
       foldGutter: true,
       indentWithTabs: true,
-      readOnly: true,
+      readOnly: false,
       gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter']
     };
 
@@ -71,12 +69,8 @@ export class MappingComponent implements OnInit {
   /* load the data on statistics and display this in a card (=readonly editor)
   /* mocked data for now
   */
-  loadStatistics() {
-    this.editorConfig = Object.assign(this.defaultEditorConfig);
-    Object.freeze(this.defaultEditorConfig);
-    
-    this.workflows.getAllFinishedExecutions(this.datasetData.datasetId, 0, 'only_validation_external').subscribe(result => {
-      
+  loadStatistics() {  
+    this.workflows.getAllFinishedExecutions(this.datasetData.datasetId, 0, 'only_validation_external').subscribe(result => {      
       let taskId: string;
       if (result['results'].length > 0) {
         taskId = result['results'][0]['metisPlugins'][0]['externalTaskId']; // should be updated after changes in validation
@@ -89,8 +83,7 @@ export class MappingComponent implements OnInit {
         let error = this.errors.handleError(err); 
         this.errorMessage = `${StringifyHttpError(error)}`;   
       });      
-    });
-    
+    });    
   }
 
   /** loadEditor
@@ -98,9 +91,6 @@ export class MappingComponent implements OnInit {
   /* fullview (whole file in one card) or not (display in different cards, based on comments in file)
   */
   loadEditor() {
-    this.editorConfigEdit = Object.assign({}, this.defaultEditorConfig);
-    this.editorConfigEdit['readOnly'] = false;
-    this.editorConfigEdit['mode'] = 'application/xml';
     this.loadXSLT('default');
   }
 
