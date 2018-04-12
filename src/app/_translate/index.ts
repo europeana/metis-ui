ef79f02b563b2a94bdb699c19c34046fb6a3b552
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2018-04-12 11:25:08 +0200

    feat/MET-802-rename-workflowlabels

--- src/app/_translate/index.ts
+++ src/app/_translate/index.ts
@@ -1,3 +1,4 @@
 export * from './lang-en';
 export * from './translate.pipe';
 export * from './translation';
+export * from './renameworkflow.pipe';
---
+++ src/app/_translate/renameworkflow.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+let workflowNames = { 
+  HTTP_HARVEST: 'Import HTTP',
+  OAIPMH_HARVEST: 'Import OAI-PMH', 
+  TRANSFORMATION: 'Transform',
+  VALIDATION_EXTERNAL: 'Validate',
+  VALIDATION_INTERNAL: 'Validate',
+  ENRICHMENT: 'Enrich'
+}; 
+
+@Pipe({
+  name: 'renameWorkflow'
+})
+
+export class RenameWorkflowPipe implements PipeTransform {
+  transform(value: string): string {
+    return workflowNames[value];
+  }  
+}
