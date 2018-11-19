d9bd221d7bb583b71c6e951e854ac8c63020c33f
Jan Heijmans <jan@egami.nl>
2018-11-19 11:44:01 +0100

    tslint in cypress webloader doesn’t seem to work, so disable it
    we still run tslint on the cypress code in `ng lint`.

--- cypress/webpack.config.js
+++ cypress/webpack.config.js
@@ -2,14 +2,6 @@ let config = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: {
-          loader: "tslint-loader",
-        },
-        enforce: "pre",
-        exclude: /node_modules/,
-      },
-      {
         test: /\.jsx?$|\.tsx?$/,
         use: {
           loader: "ts-loader",
