7a295bedfa6167b57cf7aa7b48fbd9a49413b942
Jan Heijmans <jan@egami.nl>
2018-11-20 12:36:04 +0100

    e2e: dataset page

--- cypress/integration/dataset.spec.ts
+++ cypress/integration/dataset.spec.ts
@@ -1,5 +1,9 @@
 import { setupUser, setupWorkflowRoutes } from '../support/helpers';
 
+function getHistoryRow(index: number, sel: string): Cypress.Chainable {
+  return cy.get(`.history-table tbody tr:nth-child(${index + 1}) ${sel}`);
+}
+
 context('metis-ui', () => {
   describe('dataset', () => {
     beforeEach(() => {
@@ -14,7 +18,7 @@ context('metis-ui', () => {
       ]);
     });
 
-    it('should show the dataset, general info, status', () => {
+    it('should show the dataset, general info, status, history', () => {
       cy.get('.dataset-name').contains('datasetName');
 
       cy.get('.metis-dataset-info-block dd').contains('Europeana');
@@ -23,6 +27,14 @@ context('metis-ui', () => {
 
       cy.get('.dataset-actionbar .status').contains('Preview');
       cy.get('.dataset-actionbar .status').contains('FAILED');
+
+      cy.get('.history-table tbody tr').should('have.length', 9);
+      getHistoryRow(1, 'td').contains('Preview');
+      getHistoryRow(1, 'td').contains('CANCELLED');
+      getHistoryRow(2, 'td').contains('Process Media');
+      getHistoryRow(2, 'td').contains('CANCELLED');
+      getHistoryRow(8, 'td').contains('Import OAI-PMH');
+      getHistoryRow(8, 'td').contains('FINISHED');
     });
   });
 });
