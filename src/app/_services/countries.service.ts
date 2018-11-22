e4bfcbd1d3c94fabe1649a858c709fba17a4c898
Jan Heijmans <jan@egami.nl>
2018-11-22 16:12:34 +0100

    country service: cache countries and languages

--- src/app/_services/countries.service.ts
+++ src/app/_services/countries.service.ts
@@ -1,12 +1,10 @@
-import { Observable } from 'rxjs';
-import {map} from 'rxjs/operators';
+import { ConnectableObservable, Observable } from 'rxjs';
+import { publishLast } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { apiSettings } from '../../environments/apisettings';
 
-import { AuthenticationService } from '../_services/authentication.service';
-import { StringifyHttpError } from '../_helpers';
 import { ErrorService } from './error.service';
 import { Country } from '../_models/country';
 import { Language } from '../_models/language';
@@ -15,22 +13,37 @@ import { Language } from '../_models/language';
 export class CountriesService {
 
   constructor(private http: HttpClient,
-    private errors: ErrorService,
-    private authentication: AuthenticationService) {}
+    private errors: ErrorService) {}
 
-  /** getCountries
-  /* get a list of countries
-  */
-  getCountries(): Observable<Country[]> {
+  private countries?: Observable<Country[]>;
+  private languages?: Observable<Language[]>;
+
+  private requestCountries(): Observable<Country[]> {
     const url = `${apiSettings.apiHostCore}/datasets/countries`;
     return this.http.get<Country[]>(url).pipe(this.errors.handleRetry());
   }
 
-  /** getLanguages
-  /* get a list of languages
-  */
-  getLanguages(): Observable<Language[]> {
+  getCountries(): Observable<Country[]> {
+    if (this.countries) {
+      return this.countries;
+    }
+    this.countries = this.requestCountries().pipe(publishLast());
+    (this.countries as ConnectableObservable<Country[]>).connect();
+    return this.countries;
+  }
+
+  private requestLanguages(): Observable<Language[]> {
     const url = `${apiSettings.apiHostCore}/datasets/languages`;
     return this.http.get<Language[]>(url).pipe(this.errors.handleRetry());
   }
+
+  getLanguages(): Observable<Language[]> {
+    if (this.languages) {
+      return this.languages;
+    }
+    this.languages = this.requestLanguages().pipe(publishLast());
+    (this.languages as ConnectableObservable<Language[]>).connect();
+    return this.languages;
+  }
+
 }
