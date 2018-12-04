c2e00731733a3fddd046102a9f57f196e9fb2bba
Jan Heijmans <jan@egami.nl>
2018-12-04 14:18:51 +0100

    fix unit tests: general info

--- src/app/dataset/generalinfo/generalinfo.component.spec.ts
+++ src/app/dataset/generalinfo/generalinfo.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GeneralinfoComponent } from './generalinfo.component';
 import { DatasetsService, TranslateService, ErrorService, AuthenticationService, RedirectPreviousUrl, WorkflowService } from '../../_services';
-import { MockDatasetService, MockWorkflowService, currentDataset, MockAuthenticationService, MockTranslateService } from '../../_mocked';
+import {MockDatasetService, MockWorkflowService, currentDataset, MockAuthenticationService, MockTranslateService, harvestData} from '../../_mocked';
 
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -31,17 +31,23 @@ describe('GeneralinfoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GeneralinfoComponent);
     component = fixture.componentInstance;
+    component.datasetData = currentDataset;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
-    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
   it('should try to find publication data', () => {
-    component.datasetData = currentDataset;
-    component.getDatasetInformation();
+    component.harvestPublicationData = harvestData;
     fixture.detectChanges();
-    expect(component.harvestPublicationData).toBeTruthy();
+    expect(component.harvestPublicationData).toBe(harvestData);
+    expect(component.lastPublishedRecords).toBe(842);
+    expect(component.lastPublishedDate).toBe('2018-03-30T13:53:04.762Z');
+    expect(component.viewPreview).toBe('https://metis-preview-portal-test.eanadev.org/portal/en/search?q=edm_datasetName:1_*');
+    expect(component.buttonClassPreview).toBe('');
+    expect(component.viewCollections).toBe('https://metis-publish-portal-test.eanadev.org/portal/en/search?q=edm_datasetName:1_*');
+    expect(component.buttonClassCollections).toBe('');
   });
 });
