954b9cbbdd6eccad5bcd2f3d9a6b16ac6541a9c8
Jan Heijmans <jan@egami.nl>
2018-12-10 17:44:56 +0100

    fix some imports

--- src/app/shared/loading-button/loading-button.component.spec.ts
+++ src/app/shared/loading-button/loading-button.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoadingButtonComponent } from './loading-button.component';
+import { LoadingButtonComponent } from '.';
 
 describe('LoadingButtonComponent', () => {
   let component: LoadingButtonComponent;
