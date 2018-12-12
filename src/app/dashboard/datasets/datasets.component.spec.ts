07fe143d151f60271d7b1a2f14020b10e496574d
Jan Heijmans <jan@egami.nl>
2018-12-12 10:16:40 +0100

    fix unit tests

--- src/app/dashboard/datasets/datasets.component.spec.ts
+++ src/app/dashboard/datasets/datasets.component.spec.ts
@@ -1,4 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { MockTranslateService } from '../../_mocked';
+import { TranslatePipe, TranslateService } from '../../_translate';
 
 import { DatasetsComponent } from '.';
 
@@ -8,7 +12,9 @@ describe('DatasetsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [DatasetsComponent],
+      imports: [RouterTestingModule],
+      declarations: [DatasetsComponent, TranslatePipe],
+      providers: [{ provide: TranslateService, useClass: MockTranslateService }],
     }).compileComponents();
   }));
 
