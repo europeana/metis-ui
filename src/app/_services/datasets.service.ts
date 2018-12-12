658c0402ceea1afe6a5e1ff2212db06f5ca32dd8
Jan Heijmans <jan@egami.nl>
2018-12-12 10:15:04 +0100

    favorite datasets

--- src/app/_services/datasets.service.ts
+++ src/app/_services/datasets.service.ts
@@ -1,18 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ConnectableObservable, Observable } from 'rxjs';
-import { map, publishLast, tap } from 'rxjs/operators';
+import { ConnectableObservable, forkJoin, Observable, of } from 'rxjs';
+import { catchError, map, publishLast, tap } from 'rxjs/operators';
 
 import { apiSettings } from '../../environments/apisettings';
 import { Dataset, XmlSample } from '../_models';
 
 import { ErrorService } from './error.service';
 
+const FAVORITE_IDS = 'favoriteIds';
+
 @Injectable()
 export class DatasetsService {
   datasetById: { [id: string]: Observable<Dataset> } = {};
+  favoriteIds: string[];
 
-  constructor(private http: HttpClient, private errors: ErrorService) {}
+  constructor(private http: HttpClient, private errors: ErrorService) {
+    this.favoriteIds = JSON.parse(localStorage.getItem(FAVORITE_IDS) || '[]');
+  }
 
   private requestDataset(id: string): Observable<Dataset> {
     const url = `${apiSettings.apiHostCore}/datasets/${id}`;
@@ -87,5 +92,57 @@ export class DatasetsService {
         headers: { 'Content-Type': 'application/json' },
       })
       .pipe(this.errors.handleRetry());
+  }
+
+  isFavorite(dataset: Dataset): boolean {
+    return this.favoriteIds.indexOf(dataset.datasetId) >= 0;
+  }
+
+  saveFavorites(): void {
+    localStorage.setItem(FAVORITE_IDS, JSON.stringify(this.favoriteIds));
+  }
+
+  sortFavorites(): void {
+    this.favoriteIds.sort((id1, id2) => {
+      const idNum1 = parseInt(id1, 10);
+      const idNum2 = parseInt(id2, 10);
+      if (idNum1 < idNum2) {
+        return -1;
+      } else if (idNum1 > idNum2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  addFavorite(dataset: Dataset): void {
+    if (!this.isFavorite(dataset)) {
+      this.favoriteIds.push(dataset.datasetId);
+      this.sortFavorites();
+      this.saveFavorites();
+    }
+  }
+
+  removeFavorite(dataset: Dataset): void {
+    if (this.isFavorite(dataset)) {
+      const index = this.favoriteIds.indexOf(dataset.datasetId);
+      this.favoriteIds.splice(index, 1);
+      this.saveFavorites();
+    }
+  }
+
+  getFavorites(): Observable<Dataset[]> {
+    if (this.favoriteIds.length === 0) {
+      return of([]);
+    }
+
+    const getDatasetOrUndefined = (id: string) =>
+      this.getDataset(id).pipe(catchError(() => of(undefined)));
+
+    const filterDatasets = (datasets: Array<Dataset | undefined>) =>
+      datasets.filter((dataset) => dataset) as Dataset[];
+
+    return forkJoin(this.favoriteIds.map(getDatasetOrUndefined)).pipe(map(filterDatasets));
   }
 }
--- src/app/dashboard/index.ts
+++ src/app/dashboard/index.ts
@@ -1,5 +1,6 @@
 export * from './dashboard.component';
 export * from './dashboardactions/';
+export * from './datasets/';
 export * from './executions/';
 export * from './executions/executiontable/';
 export * from './ongoingexecutions/';
