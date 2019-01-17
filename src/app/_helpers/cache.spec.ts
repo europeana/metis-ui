e65755511c26317d78ce045a7ab28ca0d5b8ddad
Jan Heijmans <jan@egami.nl>
2019-01-17 10:47:22 +0100

    fix spacing in unit test

--- src/app/_helpers/cache.spec.ts
+++ src/app/_helpers/cache.spec.ts
@@ -40,52 +40,52 @@ describe('single cache', () => {
     expect(gatherError(cache.get())).toEqual('wrong');
     expect(fn).toHaveBeenCalledTimes(3);
   });
+});
 
-  describe('keyed cache', () => {
-    it('should get the value', () => {
-      const fn = jasmine.createSpy().and.callFake((key: string) => of(`key:${key}`));
-      const cache = new KeyedCache<string>(fn);
-      expect(gatherValues(cache.get('1'))).toEqual(['key:1']);
-      expect(gatherValues(cache.get('2'))).toEqual(['key:2']);
-      expect(gatherValues(cache.get('3'))).toEqual(['key:3']);
-      expect(gatherValues(cache.get('2'))).toEqual(['key:2']);
-      expect(gatherValues(cache.get('3'))).toEqual(['key:3']);
-      expect(fn).toHaveBeenCalledTimes(3);
-    });
+describe('keyed cache', () => {
+  it('should get the value', () => {
+    const fn = jasmine.createSpy().and.callFake((key: string) => of(`key:${key}`));
+    const cache = new KeyedCache<string>(fn);
+    expect(gatherValues(cache.get('1'))).toEqual(['key:1']);
+    expect(gatherValues(cache.get('2'))).toEqual(['key:2']);
+    expect(gatherValues(cache.get('3'))).toEqual(['key:3']);
+    expect(gatherValues(cache.get('2'))).toEqual(['key:2']);
+    expect(gatherValues(cache.get('3'))).toEqual(['key:3']);
+    expect(fn).toHaveBeenCalledTimes(3);
+  });
 
-    it('should refresh the value', () => {
-      const fn = jasmine.createSpy().and.callFake((key: string) => of(`key:${key}`));
-      const cache = new KeyedCache<string>(fn);
-      expect(gatherValues(cache.get('1', true))).toEqual(['key:1']);
-      expect(gatherValues(cache.get('2', true))).toEqual(['key:2']);
-      expect(gatherValues(cache.get('3', true))).toEqual(['key:3']);
-      expect(gatherValues(cache.get('2', true))).toEqual(['key:2']);
-      expect(gatherValues(cache.get('3', true))).toEqual(['key:3']);
-      expect(fn).toHaveBeenCalledTimes(5);
-    });
+  it('should refresh the value', () => {
+    const fn = jasmine.createSpy().and.callFake((key: string) => of(`key:${key}`));
+    const cache = new KeyedCache<string>(fn);
+    expect(gatherValues(cache.get('1', true))).toEqual(['key:1']);
+    expect(gatherValues(cache.get('2', true))).toEqual(['key:2']);
+    expect(gatherValues(cache.get('3', true))).toEqual(['key:3']);
+    expect(gatherValues(cache.get('2', true))).toEqual(['key:2']);
+    expect(gatherValues(cache.get('3', true))).toEqual(['key:3']);
+    expect(fn).toHaveBeenCalledTimes(5);
+  });
 
-    it('should clear the value', () => {
-      const fn = jasmine.createSpy().and.callFake((key: string) => of(`key:${key}`));
-      const cache = new KeyedCache<string>(fn);
-      expect(gatherValues(cache.get('1'))).toEqual(['key:1']);
-      expect(gatherValues(cache.get('2'))).toEqual(['key:2']);
-      expect(gatherValues(cache.get('3'))).toEqual(['key:3']);
-      cache.clear('2');
-      expect(gatherValues(cache.get('2'))).toEqual(['key:2']);
-      expect(gatherValues(cache.get('3'))).toEqual(['key:3']);
-      expect(gatherValues(cache.get('2'))).toEqual(['key:2']);
-      expect(fn).toHaveBeenCalledTimes(4);
-    });
+  it('should clear the value', () => {
+    const fn = jasmine.createSpy().and.callFake((key: string) => of(`key:${key}`));
+    const cache = new KeyedCache<string>(fn);
+    expect(gatherValues(cache.get('1'))).toEqual(['key:1']);
+    expect(gatherValues(cache.get('2'))).toEqual(['key:2']);
+    expect(gatherValues(cache.get('3'))).toEqual(['key:3']);
+    cache.clear('2');
+    expect(gatherValues(cache.get('2'))).toEqual(['key:2']);
+    expect(gatherValues(cache.get('3'))).toEqual(['key:3']);
+    expect(gatherValues(cache.get('2'))).toEqual(['key:2']);
+    expect(fn).toHaveBeenCalledTimes(4);
+  });
 
-    it('should not cache an error', () => {
-      const fn = jasmine.createSpy().and.callFake(() => throwError('wrong'));
-      const cache = new KeyedCache<string>(fn);
-      expect(gatherError(cache.get('1'))).toEqual('wrong');
-      expect(gatherError(cache.get('2'))).toEqual('wrong');
-      expect(gatherError(cache.get('3'))).toEqual('wrong');
-      expect(gatherError(cache.get('2'))).toEqual('wrong');
-      expect(gatherError(cache.get('3'))).toEqual('wrong');
-      expect(fn).toHaveBeenCalledTimes(5);
-    });
+  it('should not cache an error', () => {
+    const fn = jasmine.createSpy().and.callFake(() => throwError('wrong'));
+    const cache = new KeyedCache<string>(fn);
+    expect(gatherError(cache.get('1'))).toEqual('wrong');
+    expect(gatherError(cache.get('2'))).toEqual('wrong');
+    expect(gatherError(cache.get('3'))).toEqual('wrong');
+    expect(gatherError(cache.get('2'))).toEqual('wrong');
+    expect(gatherError(cache.get('3'))).toEqual('wrong');
+    expect(fn).toHaveBeenCalledTimes(5);
   });
 });
