2f7c5098000f206cdb09288d4c0d67c75c9501bd
Andy MacLean <andyjmaclean@gmail.com>
2019-01-25 15:16:02 +0100

    Added helper unit tests

---
+++ src/app/_helpers/calcprogress.spec.ts
@@ -0,0 +1,31 @@
+import { WorkflowExecution } from '../_models';
+
+import { calcProgress } from './calcprogress';
+
+function makeWorkflowExecution(currIndex: number, progressStatuses: (number)[]): WorkflowExecution {
+  // tslint:disable:no-any
+  const fakePlugins: (any)[] = [];
+
+  progressStatuses.forEach((status) => {
+    fakePlugins.push({ executionProgress: { progressPercentage: status } });
+  });
+
+  return ({
+    currentPluginIndex: currIndex,
+    metisPlugins: fakePlugins,
+  } as any) as WorkflowExecution;
+}
+
+describe('calc progress', () => {
+  it('should return the exact status of the plugin when the WorkflowExecution contains a single plugin', () => {
+    expect(calcProgress(makeWorkflowExecution(0, [25]))).toBe(25);
+    expect(calcProgress(makeWorkflowExecution(0, [50]))).toBe(50);
+    expect(calcProgress(makeWorkflowExecution(0, [75]))).toBe(75);
+    expect(calcProgress(makeWorkflowExecution(0, [100]))).toBe(100);
+  });
+
+  it('should calculate an overall status based on the current plugin when multiple plugins are supplied', () => {
+    expect(calcProgress(makeWorkflowExecution(1, [100, 50]))).toBe(75);
+    expect(calcProgress(makeWorkflowExecution(3, [100, 100, 100, 0]))).toBe(75);
+  });
+});
