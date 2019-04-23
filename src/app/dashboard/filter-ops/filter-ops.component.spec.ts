87ebf1cab8608d98ad0ffe107f8454dc601e43eb
Andy MacLean <andyjmaclean@gmail.com>
2019-04-23 17:16:24 +0200

    MET-1702 Workflow filter - unit tests

--- src/app/dashboard/filter-ops/filter-ops.component.spec.ts
+++ src/app/dashboard/filter-ops/filter-ops.component.spec.ts
@@ -3,12 +3,9 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 // import { By } from '@angular/platform-browser';
 
-import {
-  createMockPipe,
-} from '../../_mocked';
+import { createMockPipe } from '../../_mocked';
 
 fdescribe('FilterOpsComponent', () => {
-
   let component: FilterOpsComponent;
   let fixture: ComponentFixture<FilterOpsComponent>;
   const testVal = 'xxx';
@@ -37,46 +34,63 @@ fdescribe('FilterOpsComponent', () => {
 
   it('tracks what values have been set', () => {
     expect(component.params['workflow'].length).toEqual(0);
-    expect(component.valueIsSet('workflow', 'VALUE 1') ).toBeFalsy();
-    expect(component.valueIsSet('workflow', 'VALUE 2') ).toBeFalsy();
+    expect(component.valueIsSet('workflow', 'VALUE 1')).toBeFalsy();
+    expect(component.valueIsSet('workflow', 'VALUE 2')).toBeFalsy();
 
     component.addParam('workflow', { value: 'VALUE 1', group: 'GROUP' }, 1, 1);
     component.addParam('workflow', { value: 'VALUE 2', group: 'GROUP' }, 1, 2);
 
-    expect(component.valueIsSet('workflow', 'VALUE 1', 1) ).toBeTruthy();
-    expect(component.valueIsSet('workflow', 'VALUE 2', 2) ).toBeTruthy();
+    expect(component.valueIsSet('workflow', 'VALUE 1', 1)).toBeTruthy();
+    expect(component.valueIsSet('workflow', 'VALUE 2', 2)).toBeTruthy();
   });
 
   it('tracks the index of set values', () => {
     expect(component.params['workflow'].length).toEqual(0);
-    expect(component.valueIsSet('workflow', 'VALUE 1') ).toBeFalsy();
-    expect(component.valueIsSet('workflow', 'VALUE 2') ).toBeFalsy();
+    expect(component.valueIsSet('workflow', 'VALUE 1')).toBeFalsy();
+    expect(component.valueIsSet('workflow', 'VALUE 2')).toBeFalsy();
 
     component.addParam('workflow', { value: 'VALUE 1', group: 'GROUP' }, 1, 1);
     component.addParam('workflow', { value: 'VALUE 2', group: 'GROUP' }, 1, 2);
 
-    expect(component.valueIndex('workflow', 'VALUE 1', 1) ).toEqual(0);
-    expect(component.valueIndex('workflow', 'VALUE 2', 2) ).toEqual(1);
+    expect(component.valueIndex('workflow', 'VALUE 1', 1)).toEqual(0);
+    expect(component.valueIndex('workflow', 'VALUE 2', 2)).toEqual(1);
   });
 
   it('can set values from inputs', () => {
-
-
     expect(component.valueIsSet('workflow', testVal, 0)).toBeFalsy();
-
     let el = document.createElement('input');
     el.value = testVal;
-
     component.restoreParamFromInput('workflow', el, 'group', 0, 0);
-    expect(component.valueIsSet('workflow', testVal, 0) ).toBeTruthy();
+    expect(component.valueIsSet('workflow', testVal, 0)).toBeTruthy();
+  });
+
+  it('can clear single values', () => {
+    expect(component.params['workflow'].length).toEqual(0);
+    component.addParam('workflow', { value: 'VALUE 1', group: 'GROUP' }, 1);
+    component.addParam('workflow', { value: 'VALUE 2', group: 'GROUP' }, 1);
+    expect(component.params['workflow'].length).toEqual(2);
+
+    component.clearParamValue('workflow', 'VALUE 1');
+    expect(component.params['workflow'].length).toEqual(1);
+    expect(component.params['workflow'][0].value).toEqual('VALUE 2');
+  });
+
+  it('can clear multiple values', () => {
+    expect(component.params['workflow'].length).toEqual(0);
+    component.addParam('workflow', { value: 'VALUE 1', group: 'GROUP' }, 1);
+    component.addParam('workflow', { value: 'VALUE 2', group: 'GROUP' }, 1);
+    expect(component.params['workflow'].length).toEqual(2);
+
+    component.clearParam('workflow');
+    expect(component.params['workflow'].length).toEqual(0);
   });
 
   it('toggles values when re-set', () => {
-    expect(component.valueIsSet('workflow', testVal) ).toBeFalsy();
-    component.toggleParamValue('workflow', {value: testVal}, 0);
-    expect(component.valueIsSet('workflow', testVal) ).toBeTruthy();
-    component.toggleParamValue('workflow', {value: testVal}, 0);
-    expect(component.valueIsSet('workflow', testVal) ).toBeFalsy();
+    expect(component.valueIsSet('workflow', testVal)).toBeFalsy();
+    component.toggleParamValue('workflow', { value: testVal }, 0);
+    expect(component.valueIsSet('workflow', testVal)).toBeTruthy();
+    component.toggleParamValue('workflow', { value: testVal }, 0);
+    expect(component.valueIsSet('workflow', testVal)).toBeFalsy();
   });
 
   it('should hide', () => {
@@ -92,5 +106,4 @@ fdescribe('FilterOpsComponent', () => {
     component.toggle();
     expect(component.showing).toBeTruthy();
   });
-
 });
