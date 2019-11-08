463859d557b74065a978967cb812da504ba525c7
Andy MacLean <andyjmaclean@gmail.com>
2019-11-08 17:46:08 +0100

    MET-2283 Search route reading / unit test

--- src/app/search-results/search-results.component.spec.ts
+++ src/app/search-results/search-results.component.spec.ts
@@ -1,25 +1,62 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SearchResultsComponent } from '.';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+
+class MockActivatedRoute extends ActivatedRoute {
+  public queryParams = of({});
+  setParams(paramsHash: { q: string }): void {
+    this.queryParams = of(paramsHash);
+  }
+}
 
 describe('SearchResultsComponent', () => {
-  let fixture: ComponentFixture<SearchResultsComponent>;
-  let component: SearchResultsComponent;
+  describe('with query param:', () => {
+    let fixture: ComponentFixture<SearchResultsComponent>;
+    beforeEach(async(() => {
+      const mar = new MockActivatedRoute();
+      mar.setParams({ q: '123' });
+      TestBed.configureTestingModule({
+        declarations: [SearchResultsComponent],
+        providers: [{ provide: ActivatedRoute, useValue: mar }]
+      }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [SearchResultsComponent]
+    beforeEach(() => {
+      fixture = TestBed.createComponent(SearchResultsComponent);
+      fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(SearchResultsComponent);
-    component = fixture.componentInstance;
+
+    it('should set the document title to the search result', () => {
+      expect(document.title).toContain('Search Results | 123');
+    });
   });
 
-  it('should create', () => {
-    fixture.detectChanges();
-    expect(component).toBeTruthy();
-  });
+  describe('without query param:', () => {
+    let fixture: ComponentFixture<SearchResultsComponent>;
+    let component: SearchResultsComponent;
 
-  it('should set the document title', () => {
-    fixture.detectChanges();
-    expect(document.title).toContain('Search Results');
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [SearchResultsComponent],
+        providers: [{ provide: ActivatedRoute, useValue: new MockActivatedRoute() }]
+      }).compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(SearchResultsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should set the document title', () => {
+      fixture.detectChanges();
+      expect(document.title).not.toContain('Search Results | 123');
+      expect(document.title).toContain('Search Results');
+    });
   });
 });
--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,3 +1,8 @@
+/** SearchResultsComponent
+/* - component for viewing search results
+/* - subscribes to ActivatedRoute instance for live query / result data
+*/
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { DocumentTitleService } from '../_services';
 
@@ -7,12 +12,32 @@ import { DocumentTitleService } from '../_services';
   styleUrls: ['./search-results.component.scss']
 })
 export class SearchResultsComponent implements OnInit {
-  constructor(private readonly documentTitleService: DocumentTitleService) {}
+  query: string;
+
+  constructor(
+    private readonly documentTitleService: DocumentTitleService,
+    private route: ActivatedRoute
+  ) {}
 
   /** ngOnInit
+  /* - set the query variable
   /* - set the document title
+  /*  - includes the query variable if available
   */
   ngOnInit(): void {
-    this.documentTitleService.setTitle('Search Results');
+    this.route.queryParams.subscribe((params) => {
+      this.documentTitleService.setTitle('Search Results');
+      const q = params.q;
+      if (q) {
+        this.query = q;
+      }
+      this.documentTitleService.setTitle(
+        ['Search Results', q]
+          .filter((x) => {
+            return x;
+          })
+          .join(' | ')
+      );
+    });
   }
 }
