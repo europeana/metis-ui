f5a8aa23e0a1f2c67c9348abcc92257fad933f22
Andy MacLean <andyjmaclean@gmail.com>
2019-11-28 16:18:06 +0100

    MET-2354 Redirect correctly to previous url after login

--- cypress/integration/signin.spec.ts
+++ cypress/integration/signin.spec.ts
@@ -2,6 +2,18 @@ import { cleanupUser } from '../support/helpers';
 import { user } from '../fixtures';
 
 context('metis-ui', () => {
+  const fillLoginFieldsAndSubmit = (submit = true) => {
+    cy.get('#email')
+      .clear()
+      .type('hello@example.com');
+    cy.get('#password')
+      .clear()
+      .type('x');
+    if (submit) {
+      cy.get('.login-btn').click();
+    }
+  };
+
   describe('signin', () => {
     afterEach(() => {
       cleanupUser();
@@ -19,8 +31,8 @@ context('metis-ui', () => {
         .blur();
       cy.get('#password')
         .clear()
-        .type('x')
-        .blur();
+        .type('x');
+
       cy.get('.error-message').contains('Please enter a valid email address');
       cy.get('.login-btn').should('be.disabled');
 
@@ -39,18 +51,12 @@ context('metis-ui', () => {
     });
 
     it('should validate the password', () => {
-      cy.get('#email')
-        .clear()
-        .type('hello@example.com')
-        .blur();
+      fillLoginFieldsAndSubmit(false);
       cy.get('#password')
-        .clear()
-        .type('x')
         .clear()
         .blur();
       cy.get('.error-message').contains('Please enter a valid password');
       cy.get('.login-btn').should('be.disabled');
-
       cy.get('#password')
         .clear()
         .type('x');
@@ -65,50 +71,30 @@ context('metis-ui', () => {
         status: 401,
         response: { errorMessage: 'Oops!' }
       });
-
-      cy.get('#email')
-        .clear()
-        .type('hello@example.com')
-        .blur();
-      cy.get('#password')
-        .clear()
-        .type('x')
-        .blur();
-      cy.get('.login-btn').click();
-
+      fillLoginFieldsAndSubmit();
       cy.get('.error-notification').contains('401 Oops!');
     });
 
     it('should login', () => {
       cy.route('POST', '/authentication/login', user);
-
-      cy.get('#email')
-        .clear()
-        .type('hello@example.com')
-        .blur();
-      cy.get('#password')
-        .clear()
-        .type('x')
-        .blur();
-      cy.get('.login-btn').click();
-
+      fillLoginFieldsAndSubmit();
       cy.url().should('contain', '/dashboard');
     });
 
     it('should show the search form when logged in', () => {
       cy.route('POST', '/authentication/login', user);
-      cy.get('#email')
-        .clear()
-        .type('hello@example.com')
-        .blur();
-      cy.get('#password')
-        .clear()
-        .type('x')
-        .blur();
-
+      fillLoginFieldsAndSubmit(false);
       cy.get('.search-form').should('have.length', 0);
       cy.get('.login-btn').click();
       cy.get('.search-form').should('have.length', 1);
+    });
+
+    it('should redirect to the originally requested url after a successful login', () => {
+      const destinationUrl = '/dataset/edit/2';
+      cy.visit(destinationUrl);
+      cy.url().should('contain', '/signin');
+      fillLoginFieldsAndSubmit();
+      cy.url().should('contain', destinationUrl);
     });
   });
 });
