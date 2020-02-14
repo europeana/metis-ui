45a79ad3e2089207490a3f05af102db0b482b96b
Andy MacLean <andyjmaclean@gmail.com>
2020-02-14 11:34:15 +0100

    MET-2416 Redirection self-reference warning

--- src/app/_translate/lang-en.ts
+++ src/app/_translate/lang-en.ts
@@ -36,7 +36,7 @@ export const LANG_EN_TRANS = Object.assign(LANG_EN_STATUS_TYPES, {
   dateCreated: 'Date Created',
   dataProvider: 'Data Provider',
   dataset: 'Dataset',
-  datasetNoSelfReference: "Dataset ID can't match current ID",
+  datasetNoSelfReference: "The dataset ID entered matches the id of the dataset being edited",
   datasetInexistent: 'Dataset ID doesn’t exist',
   datasetInformation: 'Dataset Information',
   datasetName: 'Dataset Name',
