fc9f9c312b2ec960bb0aa2338c78c4c88b892f0c
andyjmaclean <andyjmaclean@gmail.com>
2020-07-16 11:16:43 +0200

    Add unit test for error handling

--- src/app/data-polling/data-polling.component.spec.ts
+++ src/app/data-polling/data-polling.component.spec.ts
@@ -1,8 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { of, Subject } from 'rxjs';
+import { of, Subject, throwError } from 'rxjs';
 import { RouterModule } from '@angular/router';
-
 import { DataPollingComponent } from './';
 
 describe('DataPollingComponent', () => {
@@ -25,26 +24,45 @@ describe('DataPollingComponent', () => {
     component = fixture.componentInstance;
   });
 
-  it('should update data periodically and allow polling resets', fakeAsync(() => {
-    const pollFn = jasmine.createSpy().and.callFake(() => of(true));
-    const processFn = jasmine.createSpy('process');
-    const subject: Subject<boolean> = component.getDataPoller(interval, pollFn, processFn, fnEmpty);
+  describe('Normal operations', () => {
+    it('should update data periodically and allow polling resets', fakeAsync(() => {
+      const pollFn = jasmine.createSpy().and.callFake(() => of(true));
+      const processFn = jasmine.createSpy('process');
+      const subject: Subject<boolean> = component.getDataPoller(
+        interval,
+        pollFn,
+        processFn,
+        fnEmpty
+      );
 
-    [1, 2, 3, 4, 5].forEach((index) => {
-      expect(pollFn).toHaveBeenCalledTimes(index);
+      [1, 2, 3, 4, 5].forEach((index) => {
+        expect(pollFn).toHaveBeenCalledTimes(index);
+        tick(interval);
+      });
+      expect(pollFn).toHaveBeenCalledTimes(6);
+      subject.next(true);
+      expect(pollFn).toHaveBeenCalledTimes(7);
+      component.cleanup();
       tick(interval);
-    });
+    }));
 
-    expect(pollFn).toHaveBeenCalledTimes(6);
-    subject.next(true);
-    expect(pollFn).toHaveBeenCalledTimes(7);
-    component.cleanup();
-    tick(interval);
-  }));
+    it('should cleanup on destroy', fakeAsync(() => {
+      spyOn(component, 'cleanup').and.callThrough();
+      component.ngOnDestroy();
+      expect(component.cleanup).toHaveBeenCalled();
+    }));
+  });
 
-  it('should cleanup on destroy', fakeAsync(() => {
-    spyOn(component, 'cleanup').and.callThrough();
-    component.ngOnDestroy();
-    expect(component.cleanup).toHaveBeenCalled();
-  }));
+  describe('Error handling', () => {
+    it('should invoke the error handling function on error', fakeAsync(() => {
+      const pollFn = () => {
+        return throwError('mock data-poll error...');
+      };
+      const processFn = jasmine.createSpy('process');
+      const errrorFn = jasmine.createSpy('error-handler');
+      component.getDataPoller(interval, pollFn, processFn, errrorFn);
+      expect(errrorFn).toHaveBeenCalled();
+      expect(processFn).not.toHaveBeenCalled();
+    }));
+  });
 });
