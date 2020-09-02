f3cba770487ac6f6d7eba79582fbd217c0470f7e
andyjmaclean <andyjmaclean@gmail.com>
2020-09-02 14:51:16 +0200

    Added to translatables / capitalise function / cypress check

--- src/app/_services/document-title.service.ts
+++ src/app/_services/document-title.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class DocumentTitleService {
-  public setTitle(title: string): void {
-    document.title = title + ' | Metis';
+  public setTitle(title: string, capitalise = false): void {
+    let docTitle = capitalise ? title.replace(/\b[a-z]/g, l => l.toUpperCase()) : title;
+    document.title = docTitle + ' | Metis';
   }
 }
