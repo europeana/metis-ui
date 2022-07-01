772f79b65bd0f4a53a7e26669543ccc1f8b669a7
andyjmaclean <andyjmaclean@gmail.com>
2022-07-01 17:26:52 +0200

    Use AsyncSubject / update tap syntax

--- projects/metis/src/app/_helpers/cache.ts
+++ projects/metis/src/app/_helpers/cache.ts
@@ -1,5 +1,5 @@
-import { Observable, of } from 'rxjs';
-import { shareReplay, tap } from 'rxjs/operators';
+import { AsyncSubject, connectable, Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export class SingleCache<Value> {
   private observable?: Observable<Value>;
@@ -15,15 +15,21 @@ export class SingleCache<Value> {
       return this.observable;
     }
 
-    const observable = this.sourceFn().pipe(
-      tap({
-        error: () => this.clear()
-      }),
-      shareReplay(1)
+    // Create new observable.
+    const observable = connectable(
+      this.sourceFn().pipe(
+        tap({
+          error: () => this.clear()
+        })
+      ),
+      {
+        connector: () => new AsyncSubject<Value>(),
+        resetOnDisconnect: false
+      }
     );
-
     // Return local variable, as this.observable might be cleared by the connect call.
     this.observable = observable;
+    observable.connect();
     return observable;
   }
 
@@ -58,15 +64,21 @@ export class KeyedCache<Value> {
     }
 
     // Create new observable.
-    const observable = this.sourceFn(key).pipe(
-      tap({
-        error: () => this.clear(key)
-      }),
-      shareReplay(1)
+    const observable = connectable(
+      this.sourceFn(key).pipe(
+        tap({
+          error: (_) => this.clear(key)
+        })
+      ),
+      {
+        connector: () => new AsyncSubject<Value>(),
+        resetOnDisconnect: false
+      }
     );
 
     // Return local variable, as this.observableByKey[key] might be cleared by the connect call.
     this.observableByKey[key] = observable;
+    observable.connect();
     return observable;
   }
 
