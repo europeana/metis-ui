72fc4c3d089c68b9f35d880025d2bb977d850db8
andyjmaclean <andyjmaclean@gmail.com>
2022-08-05 19:18:39 +0200

    MET-4666 Tier Zero Records: Incorporate clickAwareDirective

--- projects/sandbox/src/app/app.component.ts
+++ projects/sandbox/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { apiSettings } from '../environments/apisettings';
+
+import { ClickService } from 'shared';
 
 @Component({
   selector: 'sb-root',
@@ -8,4 +10,16 @@ import { apiSettings } from '../environments/apisettings';
 })
 export class AppComponent {
   public feedbackUrl = apiSettings.feedbackUrl;
+
+  constructor(private readonly clickService: ClickService) {}
+
+  /** documentClick
+   * - global document click handler
+   * - push the clicked element to the clickService
+   * - (picked up by the click-aware directive)
+   **/
+  @HostListener('document:click', ['$event'])
+  documentClick(event: { target: HTMLElement }): void {
+    this.clickService.documentClickedTarget.next(event.target);
+  }
 }
