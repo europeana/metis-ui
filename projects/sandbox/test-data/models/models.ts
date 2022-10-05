302df1440e67cb0fed492e7bae953060d8e81246
andyjmaclean <andyjmaclean@gmail.com>
2022-10-05 11:40:26 +0200

    MET-4844 Prepare back-end for separate dataset-info model

--- projects/sandbox/test-data/models/models.ts
+++ projects/sandbox/test-data/models/models.ts
@@ -1,4 +1,10 @@
-import { Dataset } from '../../src/app/_models';
+import { Dataset, DatasetInfo } from '../../src/app/_models';
+
+export interface DatasetWithInfo extends Dataset {
+  // TODO: we maintain composite object here.  This will no longer be an override
+  // once dataset-info is moved out of Dataset(Progress) object!
+  'dataset-info': DatasetInfo;
+}
 
 export interface ProgressBurndown {
   warn: number;
