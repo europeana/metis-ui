0d76d7efbb98e14b3296996bd77bd1df1a324118
andyjmaclean <andyjmaclean@gmail.com>
2022-11-30 17:04:33 +0100

    MET-4963 Add Sandbox Footer

--- projects/sandbox/src/app/app.module.ts
+++ projects/sandbox/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TextCopyDirective } from './_directives';
 import { FormatHarvestUrlPipe, RenameStepPipe } from './_translate';
 import { CopyableLinkItemComponent } from './copyable-link-item';
 import { DatasetInfoComponent } from './dataset-info';
+import { FooterComponent } from './footer';
 import { HttpErrorsComponent } from './http-errors';
 import { NavigationOrbsComponent } from './navigation-orbs';
 import { ProblemViewerComponent } from './problem-viewer';
@@ -24,6 +25,7 @@ import { WizardComponent } from './wizard';
     TextCopyDirective,
     CopyableLinkItemComponent,
     DatasetInfoComponent,
+    FooterComponent,
     FormatHarvestUrlPipe,
     HttpErrorsComponent,
     NavigationOrbsComponent,
