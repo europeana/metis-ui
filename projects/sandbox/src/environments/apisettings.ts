efb2b7e7cf6124346eb367fa5f63b0e945af11df
andyjmaclean <andyjmaclean@gmail.com>
2023-01-24 10:54:47 +0100

    Add Dockerisation - include apisettings

---
+++ projects/sandbox/src/environments/apisettings.ts
@@ -0,0 +1,14 @@
+const getEnvVar = (key: string): string | boolean | number | null => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const env = (window as any).__env;
+  return env ? env[key] : null;
+};
+
+export const apiSettings = {
+  enableThemes: getEnvVar('enableThemes') as boolean,
+  apiHost: getEnvVar('apiHost') as string,
+  feedbackUrl: getEnvVar('feedbackUrl') as string,
+  documentationUrl: getEnvVar('documentationUrl') as string,
+  userGuideUrl: getEnvVar('userGuideUrl') as string,
+  interval: getEnvVar('interval') as number
+};
