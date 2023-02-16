08400cbec95465c8bab255717550ec9cc7c44d25
andyjmaclean <andyjmaclean@gmail.com>
2023-02-16 10:39:59 +0100

    Tweak comments / variable naming

--- projects/shared/src/lib/_mocked/mocked-renderer-2.ts
+++ projects/shared/src/lib/_mocked/mocked-renderer-2.ts
@@ -1,3 +1,6 @@
+/* Mock of @angular.core.Renderer2 object
+ * Used to access the document.body class list
+ **/
 export class MockRenderer2 {
   addClass(_: string, __: string): boolean {
     return true;
