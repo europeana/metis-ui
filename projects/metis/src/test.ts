799bdf78db92bc50f2e7495e43e5f294a76b9e9e
andyjmaclean <andyjmaclean@gmail.com>
2023-09-21 16:39:33 +0200

    Angular 15

--- projects/metis/src/test.ts
+++ projects/metis/src/test.ts
@@ -15,8 +15,6 @@ import {
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const __karma__: any;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare const require: any;
 
 // Prevent Karma from running prematurely.
 __karma__.loaded = function(): void {
@@ -27,9 +25,5 @@ __karma__.loaded = function(): void {
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
   teardown: { destroyAfterEach: false }
 });
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
 // Finally, start Karma to run the tests.
 __karma__.start();
