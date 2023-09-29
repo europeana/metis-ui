2fa53aed9cef0508b0e66f8357b2420cb5d69ba4
Jorge Ortiz <jorge.ortiz@europeana.eu>
2023-09-29 13:48:59 +0200

    MET-5489 clean up environment files

--- projects/metis/src/assets/env.js
+++ projects/metis/src/assets/env.js
@@ -2,12 +2,11 @@
 (function(window) {
   window.__env = window.__env || {};
   const env = window.__env;
-  env.apiHostCore = 'https://metis-core-rest.test.eanadev.org';
-  //env.apiHostCore = 'http://localhost:8080/metis-core';
-  env.apiHostAuth = 'https://metis-authentication-rest.test.eanadev.org';
-  env.viewPreview = 'https://metis-preview-portal-test.eanadev.org/portal/en/search?q=edm_datasetName:';
-  env.viewCollections = 'https://metis-publish-portal-test.eanadev.org/portal/en/search?q=edm_datasetName:';
-  env.maintenanceScheduleKey = 'metis-ui-test';
-  env.maintenanceScheduleUrl = 'https://raw.githubusercontent.com/jeortizquan/x/main/metis-maintenance.json';
+  env.apiHostCore = '';
+  env.apiHostAuth = '';
+  env.viewPreview = '';
+  env.viewCollections = '';
+  env.maintenanceScheduleKey = '';
+  env.maintenanceScheduleUrl = '';
   env.maintenanceItem = {};
 })(this);
