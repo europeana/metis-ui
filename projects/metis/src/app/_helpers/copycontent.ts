61ffe0a395a08f2b39ea66a9a63d77654a7a0e29
andyjmaclean <andyjmaclean@gmail.com>
2023-11-13 11:44:05 +0100

    MET-5519 Removed Deprecated ExecCommand

--- projects/metis/src/app/_helpers/copycontent.ts
+++ projects/metis/src/app/_helpers/copycontent.ts
@@ -4,23 +4,12 @@
 /* @param {string} id1 - an id, depending on type
 /* @param {string} id2 - an id, depending on type
 */
-
 export function copyExecutionAndTaskId(type: string, id1: string, id2: string): void {
-  const selBox = document.createElement('textarea');
-  selBox.style.position = 'fixed';
-  selBox.style.left = '0';
-  selBox.style.top = '0';
-  selBox.style.opacity = '0';
+  let copyValue = '';
   if (type === 'plugin') {
-    selBox.value = `externalTaskId: ${id1}, id: ${id2}`;
+    copyValue = `externalTaskId: ${id1}, id: ${id2}`;
   } else {
-    selBox.value = `id: ${id1}, ecloudDatasetId: ${id2}`;
+    copyValue = `id: ${id1}, ecloudDatasetId: ${id2}`;
   }
-
-  document.body.appendChild(selBox);
-  selBox.focus();
-  selBox.select();
-
-  document.execCommand('copy');
-  document.body.removeChild(selBox);
+  navigator.clipboard.writeText(copyValue);
 }
