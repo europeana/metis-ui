3ec2fd536c65a3c54bc76751333485a60d0501db
andyjmaclean <andyjmaclean@gmail.com>
2025-02-27 12:58:12 +0100

    MET-6384 - Tidy config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,9 +33,9 @@ export default [{
         sourceType: "module",
 
         parserOptions: {
-            project: "/home/andy/git/metis-ui/tsconfig.json",
+            project: "./tsconfig.json",
             noWatch: true,
-            tsconfigRootDir: "/home/andy/git/metis-ui",
+            tsconfigRootDir: "./",
         },
     },
 
