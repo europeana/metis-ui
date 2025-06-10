d3fff59a5d8126c31bb6e1e649f94a63436165c2
andyjmaclean <andyjmaclean@gmail.com>
2025-06-10 19:48:42 +0200

    MET-6562 User Dataset Drop-In

--- projects/sandbox/src/app/drop-in/_mocked.ts
+++ projects/sandbox/src/app/drop-in/_mocked.ts
@@ -1,53 +1,15 @@
 import { DropInModel } from './_model';
-
 const dateNow = new Date();
-
-export const modelData: Array<DropInModel> = [
-  {
-    id: '0',
-    description: 'Anon',
-    date: new Date(dateNow.getDate() - 2).toISOString()
-  },
-  {
-    id: '11111111',
-    name: 'AAAAAA',
-    description: 'The description',
-    date: new Date(dateNow.getDate() - 1).toISOString()
-  },
-  {
-    id: '2',
-    name: 'BB BBBBBBBB',
-    description: 'The description could be really long, or short or really short or just long',
-    date: dateNow.toISOString()
-  },
-  {
-    id: '3333',
-    name: 'CCC CCC CCCCC - (all one line) - CCC CCC CCC CCC ',
-    description: 'The description could be really long, or short',
-    date: new Date(dateNow.getDate() + 1).toISOString()
-  },
-  {
-    id: '444',
-    name: 'DDDD',
-    description: 'The description',
-    date: new Date(dateNow.getDate() + 2).toISOString()
-  },
-  {
-    id: '55555',
-    name: 'E'
-  }
-];
-
-const alphabet = 'ABCDEFDHIJKL';
-
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+export const modelData: Array<DropInModel> = [];
 [...Array(100).keys()].forEach((i: number) => {
-  i += 6;
-  const x = [...Array(i % 8).keys()].map(() => 'x').join('');
-
+  const letter = alphabet[i % alphabet.length];
+  const triple = `${letter}${letter}${letter}`;
+  const tripleId = `${i}${i}${i}`;
   modelData.push({
     id: `${i}`,
-    name: '' + alphabet[i < alphabet.length ? i : i % alphabet.length],
-    description: `The description (${x}) of ${i}`,
+    name: `${triple}: ${triple.toUpperCase()} ${i} / ${tripleId}`,
+    description: `The description (${letter}) of ${i}`,
     date: new Date(dateNow.getDate() + i).toISOString()
   });
 });
