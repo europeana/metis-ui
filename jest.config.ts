df7db0d432f97f9ff796dc21b98a4c396f3983e2
andyjmaclean <andyjmaclean@gmail.com>
2025-09-10 23:52:42 +0200

    MET-6712 Jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,20 @@
 import type { Config } from 'jest';
 import { createCjsPreset } from 'jest-preset-angular/presets';
 
+
+//console.log('__dirname = ' + __dirname + ', ' + rootDir);
+
 export default {
   ...createCjsPreset(),
   setupFilesAfterEnv: ['./../../../setup-jest.ts'],
   moduleNameMapper: {
     '^uuid$': 'uuid',
-    '^shared$': __dirname + '/projects/shared/src/public-api.ts'
+    '^shared$': '<rootDir>./../../../projects/shared/src/public-api.ts'
   },
-  moduleDirectories: ['node_modules', '<rootDir>/../projects/shared/lib/public-api.ts'],
+
+  //moduleDirectories: ['node_modules', '<rootDir>/../projects/shared/lib/public-api.ts'],
+  //moduleDirectories: ['node_modules', __dirname + './../projects/shared/lib/public-api.ts'],
+  moduleDirectories: ['node_modules',  '<rootDir>./../../../projects/shared/src/lib/public-api.ts'],
   transformIgnorePatterns: ['node_modules/(?!(.*.mjs$|keycloak-js))'],
   coverageReporters: ['lcov', 'html'],
   testEnvironment: './../../../FixJsDomEnvironment.ts',
