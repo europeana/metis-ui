778647abf984df4c6fe012910e2671d987c8fbd9
andyjmaclean <andyjmaclean@gmail.com>
2025-09-11 00:55:15 +0200

    MET-6712 Jest

--- jest.config.ts
+++ jest.config.ts
@@ -9,14 +9,15 @@ export default {
   setupFilesAfterEnv: ['./../../../setup-jest.ts'],
   moduleNameMapper: {
     '^uuid$': 'uuid',
-//    '^shared$': '<rootDir>./../../../projects/shared/src/public-api.ts'
-    '^shared$': __dirname + '/projects/shared/src/public-api.ts'
-    , '^keycloak-js$': __dirname +  '/node_modules/keycloak-js/lib/keycloak.d.ts'
+    //'^shared$': __dirname + '/projects/shared/src/public-api.ts',
+    //'^keycloak-js$': __dirname +  '/node_modules/keycloak-js/lib/keycloak.d.ts'
+    '^shared$': '<rootDir>./../../../projects/shared/src/public-api.ts',
+    '^keycloak-js$': '<rootDir>./../../../node_modules/keycloak-js/lib/keycloak.d.ts'
   },
 
   //moduleDirectories: ['node_modules', '<rootDir>/../projects/shared/lib/public-api.ts'],
-  moduleDirectories: ['node_modules', __dirname + '/projects/shared/src/lib/public-api.ts'],
-  //moduleDirectories: ['node_modules',  '<rootDir>./../../../projects/shared/src/lib/public-api.ts'],
+  //moduleDirectories: ['node_modules', __dirname + '/projects/shared/src/lib/public-api.ts'],
+  moduleDirectories: ['node_modules',  '<rootDir>./../../../projects/shared/src/lib/public-api.ts'],
   transformIgnorePatterns: ['node_modules/(?!(.*.mjs$|keycloak-js))'],
   coverageReporters: ['lcov', 'html'],
   testEnvironment: './../../../FixJsDomEnvironment.ts',
