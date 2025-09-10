e2f6e0e74bca8f0db3c961e8ce8584793993e06f
andyjmaclean <andyjmaclean@gmail.com>
2025-09-11 01:35:52 +0200

    MET-6712 Jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,19 +4,26 @@ import { createCjsPreset } from 'jest-preset-angular/presets';
 
 //console.log('__dirname = ' + __dirname + ', ' + rootDir);
 
+let fnm = __filename.match(/(.*\/)(.*)$/);
+let xxx = fnm ? fnm[1].slice(0, -1) : '';
+
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   ' + xxx);
+
 export default {
   ...createCjsPreset(),
   setupFilesAfterEnv: ['./../../../setup-jest.ts'],
   moduleNameMapper: {
     '^uuid$': 'uuid',
-    '^shared$': process.cwd() + '/projects/shared/src/public-api.ts',
-    '^keycloak-js$': process.cwd() + '/node_modules/keycloak-js/lib/keycloak.d.ts'
+    '^shared$': xxx + '/projects/shared/src/public-api.ts',
+    '^keycloak-js$': xxx + '/node_modules/keycloak-js/lib/keycloak.d.ts'
+//    '^shared$': process.cwd() + '/projects/shared/src/public-api.ts',
+//    '^keycloak-js$': process.cwd() + '/node_modules/keycloak-js/lib/keycloak.d.ts'
     //'^shared$': '<rootDir>./../../../projects/shared/src/public-api.ts',
     //'^keycloak-js$': '<rootDir>./../../../node_modules/keycloak-js/lib/keycloak.d.ts'
   },
 
   //moduleDirectories: ['node_modules', '<rootDir>/../projects/shared/lib/public-api.ts'],
-  moduleDirectories: ['node_modules', process.cwd() + '/projects/shared/src/lib/public-api.ts'],
+  moduleDirectories: ['node_modules', xxx + '/projects/shared/src/lib/public-api.ts'],
   //moduleDirectories: ['node_modules',  '<rootDir>./../../../projects/shared/src/lib/public-api.ts'],
   transformIgnorePatterns: ['node_modules/(?!(.*.mjs$|keycloak-js))'],
   coverageReporters: ['lcov', 'html'],
