9d56cc0f339b14275384faa4bd6a07ca52d2aae0
andyjmaclean <andyjmaclean@gmail.com>
2025-09-11 12:31:17 +0200

    MET-6712 Jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,20 +4,25 @@ import { createCjsPreset } from 'jest-preset-angular/presets';
 
 export default {
   ...createCjsPreset(),
+//  roots: ['<rootDir>'],
+
   setupFilesAfterEnv: ['./../../../setup-jest.ts'],
   moduleNameMapper: {
     '^uuid$': 'uuid',
-    '^shared$': '<rootDir>./../../../projects/shared/src/public-api.ts',
+    '^shared$': '<rootDir>./../../shared/src/public-api.ts',
 //    '^keycloak-js$': '<rootDir>/../../../node_modules/keycloak-js/lib/keycloak.d.ts'
 //    '^keycloak-js$': '<rootDir>/../../../projects/keycloak-js/lib/keycloak.d.ts'
 //    '^keycloak-js$': '/home/andy/git/metis-ui/projects/keycloak-js/lib/keycloak.d.ts'
-    '^keycloak-js$': '<rootDir>./../../../projects/keycloak-js/lib/keycloak.d.ts'
+//    '^keycloak-js$': '<rootDir>./../../keycloak-js/lib/keycloak.d.ts'
+    '^keycloak-js$': '<rootDir>./keycloak-js/lib/keycloak.d.ts'
   },
 
-  moduleDirectories: ['node_modules', '<rootDir>./../../../projects/shared/src/lib/public-api.ts'
+  moduleDirectories: ['node_modules', '<rootDir>./../../shared/src/lib/public-api.ts'
 
-, '<rootDir>./../../../projects/keycloak-js/lib/keycloak.d.ts'
+, '<rootDir>./keycloak-js/lib/keycloak.d.ts'
+//, '<rootDir>./../../keycloak-js/lib/keycloak.d.ts'
 ],
+
 //  moduleDirectories: ['node_modules', '<rootDir>/../../../projects/keycloak_local/keycloak.d.ts'],
 
   // regexp - not a glob pattern
@@ -28,23 +33,8 @@ export default {
   ],
   */
 
+  //  testMatch: [ "**/__tests__/**/*.?([mc])[jt]s?(x)", "**/?(*.)+(spec|test).?([mc])[jt]s?(x)" ],
 
-//  testMatch: [ "**/__tests__/**/*.?([mc])[jt]s?(x)", "**/?(*.)+(spec|test).?([mc])[jt]s?(x)" ],
-
-
-  //testPathIgnorePatterns: [
-  //],
-
-  // keycloak paths do not work on github
-  //testPathIgnorePatterns: [
-
-
-//    'app\\.component',
-//    'sandbox(.*)navigation',
-//    'user\\-data',
-//    'problem\\-viewer',
-
-  //],
   transformIgnorePatterns: ['node_modules/(?!(.*.mjs$|keycloak-js))'],
   coverageReporters: ['lcov', 'html'],
   testEnvironment: './../../../FixJsDomEnvironment.ts',
