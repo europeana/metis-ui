dc50af7ec9ad1c6e47d78121934d5f8d42d656db
andyjmaclean <andyjmaclean@gmail.com>
2025-09-11 11:56:25 +0200

    MET-6712 Jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,32 +1,50 @@
-import type { Config } from 'jest';
+ import type { Config } from 'jest';
 import { createCjsPreset } from 'jest-preset-angular/presets';
 
-
-//console.log('__dirname = ' + __dirname + ', ' + rootDir);
-
-//let fnm = __filename.match(/(.*\/)(.*)$/);
-//let xxx = fnm ? fnm[1].slice(0, -1) : '';
-
-//console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   ' + xxx);
 
 export default {
   ...createCjsPreset(),
   setupFilesAfterEnv: ['./../../../setup-jest.ts'],
   moduleNameMapper: {
     '^uuid$': 'uuid',
-//    '^shared$': xxx + '/projects/shared/src/public-api.ts',
-//    '^keycloak-js$': xxx + '/node_modules/keycloak-js/lib/keycloak.d.ts'
-//    '^shared$': process.cwd() + '/projects/shared/src/public-api.ts',
-//    '^keycloak-js$': process.cwd() + '/node_modules/keycloak-js/lib/keycloak.d.ts'
-    //'^shared$': '<rootDir>./../../../projects/shared/src/public-api.ts',
-    //'^keycloak-js$': '<rootDir>./../../../node_modules/keycloak-js/lib/keycloak.d.ts'
-    '^shared$': '<rootDir>/../../../projects/shared/src/public-api.ts',
-    '^keycloak-js$': '<rootDir>/../../../node_modules/keycloak-js/lib/keycloak.d.ts'
+    '^shared$': '<rootDir>./../../../projects/shared/src/public-api.ts',
+//    '^keycloak-js$': '<rootDir>/../../../node_modules/keycloak-js/lib/keycloak.d.ts'
+//    '^keycloak-js$': '<rootDir>/../../../projects/keycloak-js/lib/keycloak.d.ts'
+//    '^keycloak-js$': '/home/andy/git/metis-ui/projects/keycloak-js/lib/keycloak.d.ts'
+    '^keycloak-js$': '<rootDir>./../../../projects/keycloak-js/lib/keycloak.d.ts'
   },
 
-  moduleDirectories: ['node_modules', '<rootDir>/../../../projects/shared/src/lib/public-api.ts'],
-  //moduleDirectories: ['node_modules', xxx + '/projects/shared/src/lib/public-api.ts'],
-  //moduleDirectories: ['node_modules',  '<rootDir>./../../../projects/shared/src/lib/public-api.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>./../../../projects/shared/src/lib/public-api.ts'
+
+, '<rootDir>./../../../projects/keycloak-js/lib/keycloak.d.ts'
+],
+//  moduleDirectories: ['node_modules', '<rootDir>/../../../projects/keycloak_local/keycloak.d.ts'],
+
+  // regexp - not a glob pattern
+  // 'xxxxxxxxx' breaks it
+  /*
+  testPathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  */
+
+
+//  testMatch: [ "**/__tests__/**/*.?([mc])[jt]s?(x)", "**/?(*.)+(spec|test).?([mc])[jt]s?(x)" ],
+
+
+  //testPathIgnorePatterns: [
+  //],
+
+  // keycloak paths do not work on github
+  //testPathIgnorePatterns: [
+
+
+//    'app\\.component',
+//    'sandbox(.*)navigation',
+//    'user\\-data',
+//    'problem\\-viewer',
+
+  //],
   transformIgnorePatterns: ['node_modules/(?!(.*.mjs$|keycloak-js))'],
   coverageReporters: ['lcov', 'html'],
   testEnvironment: './../../../FixJsDomEnvironment.ts',
