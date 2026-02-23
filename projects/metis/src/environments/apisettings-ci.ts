26d0ca4fd408fa81d809d64399cca924bf77b4c4
andyjmaclean <andyjmaclean@gmail.com>
2026-02-23 11:16:05 +0100

    MET-7022 Portal Links Update

--- projects/metis/src/environments/apisettings-ci.ts
+++ projects/metis/src/environments/apisettings-ci.ts
@@ -1,6 +1,6 @@
 export const apiSettings = {
   apiHostCore: 'http://localhost:3000',
   apiHostAuth: 'http://localhost:3000',
-  viewPreview: 'http://localhost:3000/viewPreview?q=edm_datasetName:',
-  viewCollections: 'http://localhost:3000/viewCollections?q=edm_datasetName:'
+  viewPreview: '//localhost:3000/viewPreview?query=edm_datasetName:',
+  viewCollections: '//localhost:3000/viewCollections?query=edm_datasetName:'
 };
