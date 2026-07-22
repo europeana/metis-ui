a8c2ef578e226c30be912b07b2d727552d9d6a7b
andyjmaclean <andyjmaclean@gmail.com>
2026-07-22 15:36:31 +0200

    feat/MET-7197 Problem Pattern Description Updates

--- projects/sandbox/src/app/problem-viewer/problem-viewer.component.ts
+++ projects/sandbox/src/app/problem-viewer/problem-viewer.component.ts
@@ -349,8 +349,4 @@ export class ProblemViewerComponent extends SubscriptionManager {
   toggleOccurrence(occurrence: any): void {
     occurrence.affectedRecordIdsShowing = !occurrence.affectedRecordIdsShowing;
   }
-
-  getTest(): string {
-    return 'test';
-  }
 }
