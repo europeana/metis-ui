4f6c1b690c580d4632e6ba083906aaa70b503e27
andyjmaclean <andyjmaclean@gmail.com>
2026-08-27 13:14:40 +0200

    feat/MET-7177 Metis Modernisation

--- projects/metis/src/app/dataset/editor/editor.component.ts
+++ projects/metis/src/app/dataset/editor/editor.component.ts
@@ -2,7 +2,7 @@
 /*
 /* a component for wrapping ng-content in an expandable window with theme options
 */
-import { NgClass } from '@angular/common';
+import { NgClass, NgTemplateOutlet } from '@angular/common';
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { EditorConfiguration } from 'codemirror';
 import { ClassMap, SubscriptionManager } from 'shared';
@@ -16,7 +16,7 @@ import { EditorDropDownComponent } from '../editor-drop-down';
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss'],
-  imports: [NgClass, SearchComponent, EditorDropDownComponent, TranslatePipe]
+  imports: [NgClass, NgTemplateOutlet, SearchComponent, EditorDropDownComponent, TranslatePipe]
 })
 export class EditorComponent extends SubscriptionManager {
   private readonly editorPrefs = inject(EditorPrefService);
